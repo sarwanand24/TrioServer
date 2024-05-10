9858ebf7b1106fc561a446add8ece792a2abb8b2
Nikhil Dhamgay <nikhildhamgay200424@gmail.com>
2024-05-10 14:22:34 +0530

    server

--- src/routes/hotelOrders.routes.js
+++ src/routes/hotelOrders.routes.js
@@ -1,5 +1,6 @@
 import {Router} from "express";
 import { verifyUsersJWT } from "../middlewares/auth.middleware.js";
+import { placeOrder } from "../controllers/hotelOrders.controller.js";
 
 const router = Router()
 
