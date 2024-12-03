b816b2d73ced67230ee47e9727273f27bc0ec7e2
Nikhil Dhamgay <nikhildhamgay200424@gmail.com>
2024-12-03 21:38:44 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -28,6 +28,7 @@ const allowedOrigins = [
     'https://trioserver.onrender.com',  // Add the correct URLs
     'http://localhost:3000',
     'http://localhost:2024',
+    'https://trio-hotel-owner-dashboard.vercel.app/login'
 ];
 
 const corsOptions = {
